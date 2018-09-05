edb87c27090636adddc4efd9e482e8531d9383d7
andreicnica <andreic.nica@gmail.com>
2018-09-05 12:16:51 +0300

    Fix lag with throttle rate in rosbridge

--- bidirectional_rosbridge.py
+++ bidirectional_rosbridge.py
@@ -32,6 +32,7 @@ from argparse import Namespace
 import json
 from rospy_message_converter import message_converter
 
+
 def read_cfg(config_file):
     """ Parse yaml type config file. """
     with open(config_file) as handler:
@@ -78,9 +79,12 @@ class RosbridgeClient(WebSocketClient):
         }
         self.send(dumps(msg))
 
-    def subscribe_topic(self, topic, topic_type):
+    def subscribe_topic(self, topic, topic_type, queue_length=0, throttle_rate=300):
+        print (queue_length, throttle_rate)
         msg = {
             'op': 'subscribe',
+            "queue_length": queue_length,
+            "throttle_rate": throttle_rate,
             'topic': topic,
             'type': topic_type
         }
@@ -126,47 +130,46 @@ class BidirectionalRosbridge:
         self.publishers = dict({})
 
         rospy.init_node(self.name, anonymous=True)
+        args_ = [(self.local_topics, self.local_topics_type, self.configure_local_bridge),
+                (self.remote_topics, self.remote_topics_type, self.configure_remote_bridge)]
 
-        for bridge_info in self.local_topics:
-            if len(bridge_info) == 2:
-                topic_name, topic_type = bridge_info
-                topic_out = topic_name
-            elif len(bridge_info) == 3:
-                topic_name, topic_type, topic_out = bridge_info
-            else:
-                raise ValueError('Unknown format.')
+        for t, t_type, configure in args_:
+            for bridge_info in t:
+                que_len, throttle_rate = None, None
+                if len(bridge_info) == 2:
+                    topic_name, topic_type = bridge_info
+                    topic_out = topic_name
+                elif len(bridge_info) == 3:
+                    topic_name, topic_type, topic_out = bridge_info
+                elif len(bridge_info) == 5:
+                    topic_name, topic_type, topic_out, que_len, throttle_rate = bridge_info
+                else:
+                    raise ValueError('Unknown format.')
 
-            topic_type_class = get_class(*topic_type)
-            self.local_topics_type[topic_name] = topic_type_class
+                topic_type_class = get_class(*topic_type)
+                t_type[topic_name] = topic_type_class
 
-            self.configure_local_bridge(topic_name, topic_type_class, topic_out)
+                if que_len is not None and throttle_rate is not None:
+                    configure(topic_name, topic_type_class, topic_out,
+                              queue_length=que_len, throttle_rate=throttle_rate)
+                else:
+                    configure(topic_name, topic_type_class, topic_out)
 
-        for bridge_info in self.remote_topics:
-            if len(bridge_info) == 2:
-                topic_name, topic_type = bridge_info
-                topic_out = topic_name
-            elif len(bridge_info) == 3:
-                topic_name, topic_type, topic_out = bridge_info
-            else:
-                raise ValueError('Unknown format.')
-
-            topic_type_class = get_class(*topic_type)
-            self.remote_topics_type[topic_name] = topic_type_class
-
-            self.configure_remote_bridge(topic_name, topic_type_class, topic_out)
-
-    def configure_local_bridge(self, topic_name, topic_type_class, topic_out):
+    def configure_local_bridge(self, topic_name, topic_type_class, topic_out,
+                               queue_length=0, throttle_rate=300):
         self.ws.advertise_topic(topic_out, topic_type_class._type)
         rospy.Subscriber(topic_name, topic_type_class, self.ws.publish, topic_out)
 
-    def configure_remote_bridge(self, topic_name, topic_type_class, topic_out):
-        self.ws.subscribe_topic(topic_name, topic_type_class._type)
+    def configure_remote_bridge(self, topic_name, topic_type_class, topic_out,
+                                queue_length=0, throttle_rate=300):
+        self.ws.subscribe_topic(topic_name, topic_type_class._type, queue_length, throttle_rate)
         self.publishers[topic_name] = rospy.Publisher(topic_out,
                                                       topic_type_class,
                                                       queue_size=self.queue_size)
 
     def new_message(self, topic_name, msg):
-        data = message_converter.convert_dictionary_to_ros_message(self.remote_topics_type[topic_name]._type, msg)
+        data = message_converter.convert_dictionary_to_ros_message(
+            self.remote_topics_type[topic_name]._type, msg)
         self.publishers[topic_name].publish(data)
 
     def run(self):
