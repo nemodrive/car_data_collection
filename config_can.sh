5a24ee72e91e5cd79dc501190ca0e03a4eb0d912
SanduIrina <sandu.irina2@gmail.com>
2018-12-12 12:53:03 +0200

    fixed small bugs

--- config_can.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Setup can interface (Test with candump can0)
-
-sudo ls /dev/ttyUSB*
-sudo slcan_attach -f -s6 -o /dev/ttyUSB$1
-sleep 1
-sudo slcand -S 1000000 ttyUSB$1 can$1
-sudo ifconfig can$1 up
